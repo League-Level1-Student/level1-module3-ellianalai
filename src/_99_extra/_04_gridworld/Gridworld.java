b81287ea4dadc6d2936d61f1e25f31c3ea794a93
league <matthewseul>
2024-01-13 11:30:50 -0800

    Finishing gridworld

--- src/_99_extra/_04_gridworld/Gridworld.java
+++ src/_99_extra/_04_gridworld/Gridworld.java
@@ -1,32 +1,72 @@
 package _99_extra._04_gridworld;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.util.Random;
 
 import info.gridworld.actor.Actor;
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
+import info.gridworld.world.World;
 
 public class Gridworld {
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
+	
+	World world = new World();
+	
+	Random rand = new Random();
+	int row = rand.nextInt(9);
+	int column = rand.nextInt(8);
 	
 	
-	public Gridworld() {
+	Bug bug = new Bug();
+	
+	
+	Flower flower = new Flower();
+	
+	
+
+	Location location = new Location(row, column);
+	
+	
+	Location location2 = new Location(row, column+1);
+	Location location3 = new Location(row, column-1);
+	
+	
+	
+
+	public void run() {
+		world.show();
+		for(int k = 0; k<10; k++) {
+		for(int i=0; i<10; i++) {
+			Location location = new Location(k, i);
+			if(i%2==0) {
+				flower.setColor(Color.red);
+			}
+			else {
+				flower.setColor(Color.white);
+			}
+			
+			world.add(location, flower);
+		}
 		
-	}
-	public void show() {
+		}
 		
-	}
-	
-	public void add(Location loc, Actor occ) {
 		
+		
+		/*bug.setColor(Color.cyan);
+		bug.turn();
+		world.add(location, bug);
+		world.add(location2, flower);
+		world.add(location3, flower);
+		*/
 	}
 	
 	
 	
 	
-	public static void main (String [] args) {
-		Gridworld gridworld = new Gridworld();
-		gridworld.show();
-	}
+	
+	
 }
+
+	
---
+++ src/_99_extra/_04_gridworld/GridworldRunner.java
@@ -0,0 +1,9 @@
+package _99_extra._04_gridworld;
+
+
+
+public class GridworldRunner {
+	public static void main (String[] args) {
+			new Gridworld().run();
+	}
+}

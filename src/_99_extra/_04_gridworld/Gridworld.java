024c22eb7c655125f60e5b3aad2a60493a367640
lvl0_module2 <zoey.lestyk@gmail.com>
2024-01-12 18:28:51 -0800

    Working on Gridworld

---
+++ src/_99_extra/_04_gridworld/Gridworld.java
@@ -0,0 +1,32 @@
+package _99_extra._04_gridworld;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import info.gridworld.actor.Actor;
+import info.gridworld.grid.Location;
+
+public class Gridworld {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	
+	
+	public Gridworld() {
+		
+	}
+	public void show() {
+		
+	}
+	
+	public void add(Location loc, Actor occ) {
+		
+	}
+	
+	
+	
+	
+	public static void main (String [] args) {
+		Gridworld gridworld = new Gridworld();
+		gridworld.show();
+	}
+}
